9367c428df9f7b638d932f5c5c1914dd3727a14a
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-03-28 17:16:23 +0000

    trapezoid area

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates wage.
+ */
+function calculateAreaOfTrapezoid() {
+  // input
+  const aBaseOfTrapezoid = parseFloat(document.getElementById("a-base").value)
+  const bBaseOfTrapezoid = parseFloat(document.getElementById("b-base").value)
+  const heightOfTrapezoid = parseFloat(document.getElementById("height").value)
+
+  // process
+  const areaOfTrapezoid =
+    [(aBaseOfTrapezoid + bBaseOfTrapezoid) / 2] * heightOfTrapezoid
+
+  // output
+  document.getElementById("answer").innerHTML =
+    "Area is: " + areaOfTrapezoid + " cm²"
+}
